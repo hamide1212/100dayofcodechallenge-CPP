7a435b41718a578a683f7943a2dedfc0f7aa00c4
hamide1212 <abdelhamidmhnitich@gmail.com>
2018-10-01 14:11:22 +0200

    the first code of the challenge

---
+++ 1st day.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+int Days = 100; //100 Days of code
+int main()
+{
+  while(Days >= 0){ //while it's not the 1st january
+    cout <<"challenge Mode">>; //write "challenge mode"
+    Days--; //decrement Days By one
+}
+    cout <<"Time For Another Challenge">>; //when the challenge ends there will be time for another one
+}
